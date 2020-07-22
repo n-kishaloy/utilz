790a4cb85adfb40afb4b42e9055ed49f991d8191
Kishaloy Neogi <nkishaloy@yahoo.com>
2020-07-22 01:15:14 +0100

    Version 0.0.1

--- src/Utilz/Numeric/Optima.hs
+++ src/Utilz/Numeric/Optima.hs
@@ -31,9 +31,9 @@ newtRaph f x = Just (0,x) >>= findSoln >>= (\(n,x) -> Just x) where
   findSoln :: (Int, Double) -> Maybe (Int, Double)
   findSoln (100, _) = Nothing
   findSoln (n,x0) 
-    | f0'   =~ 0.0  = Nothing 
-    | e1    =~ 0.0  = Just (n,x0-e1) 
-    | otherwise     = Just (n+1,x0-e1) >>= findSoln 
+    |f0'   =~ 0.0  = Nothing 
+    |e1    =~ 0.0  = Just (n,x0-e1) 
+    |otherwise     = Just (n+1,x0-e1) >>= findSoln 
     where 
       e1=f0/f0'; f0 = f x0; f0'= ((f (x0 + 1e-5)) - f0)/1e-5 
 
@@ -43,9 +43,9 @@ newtRP f x = do
     findSoln :: (Int, Double) -> Maybe (Int, Double)
     findSoln (100, _) = Nothing
     findSoln (n,x0) 
-      | f0'   =~ 0.0  = Nothing 
-      | e1    =~ 0.0  = Just (n,x0-e1) 
-      | otherwise     = Just (n+1,x0-e1) >>= findSoln 
+      |f0'   =~ 0.0  = Nothing 
+      |e1    =~ 0.0  = Just (n,x0-e1) 
+      |otherwise     = Just (n+1,x0-e1) >>= findSoln 
       where 
         e1=f0/f0'; f0 = f x0; f0'= ((f (x0 + 1e-5)) - f0)/1e-5 
   (n,x) <- findSoln (0,x) -- ==let v=(0,x)==v <- return (0,x) Sugar>>Desugar
@@ -63,8 +63,8 @@ conjGradPR f v0 = findSoln 0 d0 d0 v0 where
 
   findSoln 150 _ _ _   = Nothing
   findSoln m d0 p0 x0
-    | p_sq < 1e-8   = Just x0
-    | otherwise     = do
+    |p_sq < 1e-8   = Just x0
+    |otherwise     = do
       x1 <- lineOptima f x0 d0
       let 
         p1 = Nu.negGrad f x1
@@ -84,11 +84,11 @@ lineSearch f av bv la lb = findSoln 0 la x1 x2 lb fa f1 f2 fb
   x1=gr*la+gs*lb; x2=gs*la+gr*lb; fa=fx la; f1=fx x1; f2=fx x2; fb=fx lb
 
   findSoln n xa x1 x2 xb fa f1 f2 fb 
-    | n == nx   = Just $ aBx av bv $ if f1 < f2 then x1 else x2
-    | f1 > f2   = let xd = gs*x1 + gr*xb
-                  in findSoln (n+1) x1 x2 xd xb f1 f2 (fx xd) fb 
-    | otherwise = let xd = gr*xa + gs*x2
-                  in findSoln (n+1) xa xd x1 x2 fa (fx xd) f1 f2 
+    |n == nx   = Just $ aBx av bv $ if f1 < f2 then x1 else x2
+    |f1 > f2   = let xd = gs*x1 + gr*xb
+                 in findSoln (n+1) x1 x2 xd xb f1 f2 (fx xd) fb 
+    |otherwise = let xd = gr*xa + gs*x2
+                 in findSoln (n+1) xa xd x1 x2 fa (fx xd) f1 f2 
 
 bPhase :: (DVec -> Double) -> DVec -> DVec -> Maybe (Double, Double)
 bPhase f av zv = if fm > (fx 0.0) then Nothing else findSoln 0 0.0 m b d fm fb
@@ -97,6 +97,6 @@ bPhase f av zv = if fm > (fx 0.0) then Nothing else findSoln 0 0.0 m b d fm fb
 
   findSoln 100 _ _ _ _ _ _ = Nothing
   findSoln n a m b d fm fb
-    | fb > fm   =   Just (a, b)
-    | otherwise =   findSoln (n+1) m b b' d' fb (fx b') 
+    |fb > fm   =   Just (a, b)
+    |otherwise =   findSoln (n+1) m b b' d' fb (fx b') 
     where d' = d * 2.0; b' = b + d'
